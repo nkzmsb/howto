870dbdfcb3ea8a0e1358826a807ee684a973427e
nkzmsb <mkzmsb.aou@gmail.com>
2021-02-24 23:56:33 +0900

    delete file output
    basicで扱うには少し荷が重い。まずは一番単純な例を。

--- log/basic_log.py
+++ log/basic_log.py
@@ -3,10 +3,6 @@ import time
 
 LOGFILE_NAME = "log/basic_log"
 
-# logファイルの初期化
-with open(LOGFILE_NAME, mode="w"):
-    pass
-
 print("===start: not use logging===")
 # logレベルの設定
 MODE = "INFO"
@@ -18,8 +14,7 @@ for i in range(5): # 適当な繰り返しの処理を再現
     if MODE == "DEBUG" or MODE == "INFO":
         print("a:{}, b:{}".format(a,b))
     if MODE == "INFO":
-        with open(LOGFILE_NAME, mode='a') as f:
-            f.write("c:{}\n".format(c))
+        print("c:{}".format(c))
     
 print("===start: use logging===")
 # logレベルの設定
@@ -30,7 +25,8 @@ for i in range(5): # 適当な繰り返しの処理を再現
     c = a + b
     time.sleep(1) # 適当な処理時間を再現
 
+    # DEBUGモード時の処理
     logging.debug("a:{}, b:{}".format(a,b))
 
-
-logging.warning("warning")
+    # INFOモード時の処理
+    logging.info("c:{}".format(c))
