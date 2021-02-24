04ec251cef624a1cacaf04d9a9f1adb0aa44e236
nkzmsb <mkzmsb.aou@gmail.com>
2021-02-24 23:58:16 +0900

    fix comment etc,

--- log/basic_log.py
+++ log/basic_log.py
@@ -1,16 +1,16 @@
 import logging
 import time
 
-LOGFILE_NAME = "log/basic_log"
-
 print("===start: not use logging===")
 # logレベルの設定
 MODE = "INFO"
 for i in range(5): # 適当な繰り返しの処理を再現
+    # 適当な処理・処理時間を再現
     a = i
     b = i * 2
     c = a + b
-    time.sleep(1) # 適当な処理時間を再現
+    time.sleep(1)
+
     if MODE == "DEBUG" or MODE == "INFO":
         print("a:{}, b:{}".format(a,b))
     if MODE == "INFO":
@@ -20,10 +20,11 @@ print("===start: use logging===")
 # logレベルの設定
 logging.basicConfig(level=logging.INFO)
 for i in range(5): # 適当な繰り返しの処理を再現
+    # 適当な処理・処理時間を再現
     a = i
     b = i * 2
     c = a + b
-    time.sleep(1) # 適当な処理時間を再現
+    time.sleep(1)
 
     # DEBUGモード時の処理
     logging.debug("a:{}, b:{}".format(a,b))
