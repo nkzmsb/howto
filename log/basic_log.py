217075ceb67b94d9ce6bde659b28a9c1a3518512
nkzmsb <mkzmsb.aou@gmail.com>
2021-02-24 23:58:52 +0900

    comment out "not use logging"

--- log/basic_log.py
+++ log/basic_log.py
@@ -1,20 +1,20 @@
 import logging
 import time
 
-print("===start: not use logging===")
-# logレベルの設定
-MODE = "INFO"
-for i in range(5): # 適当な繰り返しの処理を再現
-    # 適当な処理・処理時間を再現
-    a = i
-    b = i * 2
-    c = a + b
-    time.sleep(1)
-
-    if MODE == "DEBUG" or MODE == "INFO":
-        print("a:{}, b:{}".format(a,b))
-    if MODE == "INFO":
-        print("c:{}".format(c))
+# print("===start: not use logging===")
+# # logレベルの設定
+# MODE = "INFO"
+# for i in range(5): # 適当な繰り返しの処理を再現
+#     # 適当な処理・処理時間を再現
+#     a = i
+#     b = i * 2
+#     c = a + b
+#     time.sleep(1)
+# 
+#     if MODE == "DEBUG" or MODE == "INFO":
+#         print("a:{}, b:{}".format(a,b))
+#     if MODE == "INFO":
+#         print("c:{}".format(c))
     
 print("===start: use logging===")
 # logレベルの設定
