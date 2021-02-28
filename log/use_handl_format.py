a4a22055666808612500778a5c939edc3c38a463
nkzmsb <mkzmsb.aou@gmail.com>
2021-02-28 23:51:57 +0900

    add datefmt

--- log/use_handl_format.py
+++ log/use_handl_format.py
@@ -3,11 +3,13 @@ import logging
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# create formatter
+formatter = logging.Formatter('%(asctime)s\t%(name)-12s\t%(module)s\t%(levelname)-8s\t%(message)s'
+                              # , datefmt='%y%m%d_%H:%M:%S' # asftimeの形式を指定することも可能
+                              )
+
 # create console handler
 handler = logging.StreamHandler()
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # add formatter to ch
 handler.setFormatter(formatter)
