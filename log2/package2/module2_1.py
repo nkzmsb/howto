933fd98adb035b141a6bc4d4dadee615d2c9f69d
nkzmsb <mkzmsb.aou@gmail.com>
2021-03-14 00:37:21 +0900

    it works but needs to be improve

--- log2/package2/module2_1.py
+++ log2/package2/module2_1.py
@@ -4,4 +4,5 @@ logger=logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 def func2_1():
-    logger.error("error log")
+    logger.error({"trace" : True
+                  , "message" : "error log"})
