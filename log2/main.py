08d7c1fa28f33f2ba328c20045838066241f6cc3
nkzmsb <mkzmsb.aou@gmail.com>
2021-03-20 22:51:32 +0900

    add unxepected log

--- log2/main.py
+++ log2/main.py
@@ -7,11 +7,15 @@ from package1.module1_1 import func1_1_1
 from package1.package1a.module1a_1 import func1a_1_1
 from package2.module2_1 import func2_1
 
-MODE = "set_file_filter"
+MODE = "set_root"
 
 if __name__=="__main__":
     
-    if MODE == "set_root":
+    if MODE == "unexpected_log":
+        from package0.module0 import func0
+        func0()
+    
+    elif MODE == "set_root":
         logging.basicConfig(level=logging.INFO
                             , format = '%(asctime)s\t%(name)-12s\t%(funcName)s\t%(levelname)-8s\t%(message)s'
                             , handlers = [logging.StreamHandler()])
@@ -42,3 +46,7 @@ if __name__=="__main__":
     func1_1_1()
     func1a_1_1()
     func2_1()
+    
+    
+    from package0.module0 import func0
+    func0()
---
+++ log2/package0/module0.py
@@ -0,0 +1,11 @@
+# 最終手段ハンドラを確認するためのモジュール
+
+import logging
+
+logger=logging.getLogger(__name__)
+
+# Note
+# any Handler is added to logger
+
+def func0():
+        logger.error("module0 : without_handler")
